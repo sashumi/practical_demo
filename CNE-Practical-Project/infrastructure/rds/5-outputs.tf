812bb94601d7fdcf4dac4b69600b2b211a5af120
sashumi <55672042+sashumi@users.noreply.github.com>
2022-08-29 00:39:05 +0100

    test

--- CNE-Practical-Project/infrastructure/rds/5-outputs.tf
+++ CNE-Practical-Project/infrastructure/rds/5-outputs.tf
@@ -1,7 +1,7 @@
 output "prod_rds_hostname" {
-  value = module.rds_production.this_db_instance_endpoint
+  value = module.rds_production.endpoint
 }
 
 output "test_rds_hostname" {
-  value = module.rds_test.this_db_instance_endpoint
+  value = module.rds_test.endpoint
 }
